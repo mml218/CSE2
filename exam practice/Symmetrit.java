9ad4ea9a5646bf55ec6b6227ca5e1682ee897fb2
mml218 <mml218@lehigh.edu>
2015-02-20 19:32:50 +0000

    added SpaceExploration

---
+++ exam practice/Symmetrit.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Symmetrit {
+            
+    public static void main(String blahblhblah []) {               
+            
+    int k =50;
+    int j =12;
+    
+    if (k/j > 2 && k%j > 1){
+        k++;
+        if(j%k > 14) {
+            k++;
+            j++;
+        }
+    else {j--;}
+        
+        
+    
+        
+    }
+    else {k/=2;}
+    System.out.println(j+" "+k);
+
+    String testingone = "Hello";
+    //char testingtwo = (char) testingone;
+
+    //System.out.println(testingtwo);
+    int wes = 112;
+    
+    System.out.printf("k = %2.5f\n", (double)wes);
+
+
+    }
+}
